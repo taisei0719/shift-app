d056426c2d030a7219f33950166e16443985dee1
taisei0719 <taisei_10.01@icloud.com>
2025-10-17 01:12:58 +0900

    9

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   plugins: {   
-    'tailwindcss': {},  // PostCSSにTailwind CSSプラグインを読み込ませる（キーは文字列 'tailwindcss'）   
+    '@tailwindcss/postcss': {},  // PostCSSにTailwind CSSプラグインを読み込ませる（キーは文字列 'tailwindcss'）   
     'autoprefixer': {},  // ベンダープレフィックス自動追加用のAutoprefixerも追加するのが標準
   },
 }
