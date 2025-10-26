87155395eddfddce2d406991aaa60400ec2275ea
taisei0719 <taisei_10.01@icloud.com>
2025-10-26 11:58:55 +0900

    a

--- mobile/lib/screens/home_screen.dart
+++ mobile/lib/screens/home_screen.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../repositories/auth_repository.dart';
-import '../../widgets/app_scaffold.dart';
+import '../widgets/app_scaffold.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
